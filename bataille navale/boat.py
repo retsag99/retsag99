9863b7adffcc8cae6c667e4b5c2d473d1b87aca6
retsag99 <127136327+retsag99@users.noreply.github.com>
2023-06-15 12:33:13 +0200

    Delete bataille navale directory

--- bataille navale/boat.py
+++
@@ -1,6 +0,0 @@
-import pygame
-
-boat_dict={"platforme_touche":["platforme_touche",pygame.image.load("images\\platforme_non_touche.png"),pygame.image.load("images\\platforme_touche.png"),0],
-           "petit_bateau":["images\platforme_touche",0]}
-
-print(boat_dict)
